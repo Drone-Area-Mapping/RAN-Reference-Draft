8865d38be617c2a7369bb2bd56a843760d0b0195
Floris <florisvdplas@live.com>
2021-09-21 16:17:33 +0200

    made the most beautiful piece of code on earth, stitches images flawlessly and only requires the filepath of the image folder as an argument, counts the amount of images on its own

--- RAN-Reference-Draft/RAN-Reference-Draft.cpp
+++ RAN-Reference-Draft/RAN-Reference-Draft.cpp
@@ -1,6 +1,75 @@
-#include <iostream>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/stitching.hpp"
+#include "opencv2/opencv.hpp"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <sys/types.h>
+#include <stdio.h>
+#include <cstdio>
+#include <memory>
+#include <stdexcept>
+
+
+using namespace std;
+using namespace cv;
+
+vector<Mat> imgs;
+string execute(const char* cmd) {
+    char buffer[128];
+    std::string result = "";
+    FILE* pipe = _popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try {
+        while (fgets(buffer, sizeof buffer, pipe) != NULL) {
+            result += buffer;
+        }
+    }
+    catch (...) {
+        _pclose(pipe);
+        throw;
+    }
+    _pclose(pipe);
+    return result;
 }
+
+int main(int argc, char* argv[])
+{
+
+    String path = "D:\\OneDrive - Avans Hogeschool\\Drone Area Mapping\\10. Example_data\\example_images2\\";
+
+    cout << String("test") << endl;
+    //cout << execute(String("cd " + path + " & powershell \"Get-ChildItem | Measure-Object | %{$_.Count}\"").c_str()) << endl;
+    int fileCounter = 0;
+    stringstream numOfFiles(execute(String("cd " + path + " & powershell \"Get-ChildItem | Measure-Object | %{$_.Count}\"").c_str()));
+    numOfFiles >> fileCounter;
+    cout << "fileCounter: " << fileCounter << endl;
+
+    for (int i = 1; i < fileCounter; ++i)
+    {
+        String file = to_string(i + 1) + ".jpg";
+        Mat img = imread(path+file);
+        if (img.empty())
+        {
+            cout << "Can't read image '" << argv[i] << "'\n";
+            return -1;
+        }
+        imgs.push_back(img);
+    }
+
+    Mat pano;
+
+    Ptr<Stitcher> stitcher = Stitcher::create(Stitcher::SCANS);
+    Stitcher::Status status = stitcher->stitch(imgs, pano);
+
+    if (status != Stitcher::OK)
+    {
+        cout << "Can't stitch images\n";
+        return -1;
+    }
+    imwrite(path+"result.jpg", pano);
+    exit(0);
+}
+
